9c9bb8dc185f7e3bf2e23aa159a6cd8becbc20ba
alishbaniya <alishbaniya@outlook.com>
2018-05-12 16:04:51 +0530

    changes

--- Tabs/App.xaml.cs
+++ Tabs/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace Tabs
 {
@@ -13,7 +16,8 @@ namespace Tabs
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            AppCenter.Start("android=d089b55e-d828-4290-8064-047c3a75e991;" + "uwp={Your UWP App secret here};" + "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
+
         }
 
         protected override void OnSleep()
